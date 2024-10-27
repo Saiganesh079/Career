256451216cf453c53a9cc39a76813e12cf4f6772
Saiganesh079 <150430634+Saiganesh079@users.noreply.github.com>
2024-10-27 12:28:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,13 @@ if query:
     process_user_input(query)
 
 
-
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 
