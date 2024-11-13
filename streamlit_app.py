4da94c4fdf7d46e32d27c580b867335c49f1fa41
Saiganesh079 <150430634+Saiganesh079@users.noreply.github.com>
2024-11-13 23:05:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ st.markdown("""
         }
     </style>
     <div class="navbar">
-        <span class="pursuit-button">Pursuit</span> <!-- Changed to span -->
+        <span class="pursuit-button" onclick="window.location.href='/https://www.instagram.com/spacenaturestencil/';">Pursuit</span>
     </div>
 """, unsafe_allow_html=True)
 
